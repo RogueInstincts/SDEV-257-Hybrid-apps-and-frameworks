5e4980ac8c83db519ec8a64b4bfa100b818c5a27
RogueInstincts <150695685+RogueInstincts@users.noreply.github.com>
2025-03-27 12:19:19 -0400

    Module 02 Assignment 2 - Star Wars

---
+++ M02-A2-StarWars/App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from 
+  "@react-navigation/native";
+import { createDrawerNavigator } from 
+  "@react-navigation/drawer";
+import { createBottomTabNavigator } from 
+  "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
+import Films from "./Films";
+import Planets from "./Planets";
+import Ships from "./Ships";
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {Platform.OS === "ios" && (
+        <Tab.Navigator>
+          <Tab.Screen name="Films" component={Films} />
+          <Tab.Screen name="Planets" component={Planets} />
+          <Tab.Screen name="Ships" component={Ships} />
+          </Tab.Navigator>
+        )}
+        {Platform.OS == "android" && (
+          <Drawer.Navigator>
+            <Drawer.Screen name="Films" component={Films} />
+            <Drawer.Screen name="Planets" component={Planets} />
+            <Drawer.Screen name="Ships" component={Ships} />
+            </Drawer.Navigator>
+          )}
+          </NavigationContainer>
+          );
+}
