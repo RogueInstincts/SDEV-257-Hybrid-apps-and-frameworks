b797c41d888fc870fcc206df93d88a1a11d49230
RogueInstincts <150695685+RogueInstincts@users.noreply.github.com>
2025-04-23 13:31:27 -0400

    Update StarWars app

--- StarWars/App.js
+++ StarWars/App.js
@@ -1,17 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react"; 
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import NetInfo from "@react-native-community/netinfo";
 import { Platform } from "react-native";
 import Films from "./Films";
 import Planets from "./Planets";
 import Ships from "./Ships";
+import styles from './styles';
+import { View, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
+const connectedMap = {
+  none: "Disconnected",
+  unknown: "Disconnected",
+  wifi: "Connected",
+  cell: "Connected",
+  mobile: "Connected",
+};
+
 export default function App() {
-  return (
+  const [connected, setConnected] = useState("");
+
+  useEffect(() => {
+    function onNetworkChange(connection) {
+      setConnected(connectedMap[connection.type]);
+    }
+
+    const unsubscribe = NetInfo.addEventListener(onNetworkChange);
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (connected == "Disconnected") {
+    return (
+    <View style={styles.centeredView}>
+      <Text style={styles.text}>Sorry, cannot connect to the internet</Text>
+    </View>
+  );
+  }
+  else {
+    return (
     <NavigationContainer>
       {Platform.OS === "ios" && (
         <Tab.Navigator>
@@ -29,4 +63,5 @@ export default function App() {
       )}
     </NavigationContainer>
   );
+  }
 }
